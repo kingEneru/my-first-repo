bb95ac10c121affc79a8676d626171c3c27ee5bf
Ethan <48342637+kingEneru@users.noreply.github.com>
2026-04-14 17:57:13 +0800

    Update download-cartoon.py

--- python/download-cartoon.py
+++ python/download-cartoon.py
@@ -234,8 +234,6 @@ class NotificationDB:
             SUPABASE_KEY
         )
         # table = 'wechat_notification_records' or 'download_records'
-    # ----------------------------------
-    # 下载动漫处理逻辑
     def process_cartoon(self, title:str , magnet:str, jishu: int, message_title:str, content: str):
         if not self.has_already_downloaded(jishu):
             print(f'检测到{title} 最新集: {jishu}\n磁力链接获取成功:\n{magnet}')
@@ -268,48 +266,6 @@ class NotificationDB:
         print('下载记录已经写入DB, 成功!')
         return result.data
 
-    # end
-    # -----------------------------------
-    def has_notification_today(self, date_str: str) -> bool:
-        """检查今天是否已发送通知"""
-        result = self.supabase.table('wechat_notification_records') \
-            .select("*") \
-            .eq('date', date_str) \
-            .execute()
-        return len(result.data) > 0
-
-    def add_notification(self, date_str: str, time_str: str, content: str):
-        """添加通知记录"""
-        data = {
-            'date': date_str,
-            'time': time_str,
-            'content': content
-        }
-        result = self.supabase.table('wechat_notification_records').insert(data).execute()
-        return result.data
-
-    def delete_notification(self, date_str: str):
-        """删除通知记录"""
-        result = self.supabase.table('wechat_notification_records') \
-            .delete() \
-            .eq('date', date_str) \
-            .execute()
-        return result.data
-
-    def process_notification(self, current_date_str: str, current_time_str: str, content: str = '淘宝下单纸巾'):
-        """处理通知逻辑"""
-        if not self.has_notification_today(current_date_str):
-            self.add_notification(current_date_str, current_time_str, content)
-            send_wechat_notification(message_title='淘宝下单纸巾',content='第一单20-23点，第二单任意时间，第三单11点之前')
-            print(f"✅ 已发送通知: {content}")
-            # 如果确实需要立即删除，取消下面的注释
-            # self.delete_notification(current_date_str)
-            # print("📝 已删除通知记录")
-            return True
-        else:
-            print(f"⏭️ 今天已发送过下单通知，跳过")
-            return False
-
 if __name__ == '__main__':
     res = {
         'cartoon_name': '',
@@ -328,8 +284,6 @@ if __name__ == '__main__':
     today = beijing_time.weekday()
     current_date_str = beijing_time.strftime("%Y-%m-%d")
     current_time_str = beijing_time.strftime("%H:%M:%S")
-    if current_time_str > '20:01:00':
-        db.process_notification(current_date_str, current_time_str, '淘宝下单纸巾')
 
     if today + 1 == 2 and current_time_str >= '10:30:00':
         # 吞噬星空每周二更新, 10点
